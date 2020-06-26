d67af66fba4cfbd054c45d3cbe91e3fb186de720
koyaryo <rytr1208@docomo.ne.jp>
2020-06-26 17:21:53 +0900

    Updates

---
+++ precache-manifest.42ea438f6fba502409f909331c3f5a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af02cea1c2b4b85a9c507f2cc679a9b",
+    "url": "/sample2/index.html"
+  },
+  {
+    "revision": "6a4a943bd2bd83f6fc98",
+    "url": "/sample2/static/css/main.488bfb3c.chunk.css"
+  },
+  {
+    "revision": "e7284a8a8761ff3fce21",
+    "url": "/sample2/static/js/2.97857a2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample2/static/js/2.97857a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4a943bd2bd83f6fc98",
+    "url": "/sample2/static/js/main.b66b218b.chunk.js"
+  },
+  {
+    "revision": "18e1964361fbc66d3f47",
+    "url": "/sample2/static/js/runtime-main.d4fb0fff.js"
+  }
+]);
